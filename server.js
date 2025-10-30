7daea30ac915f2f9d0f21b25c9f708dd6328f6e7
Your Name <you@example.com>
2025-10-30 20:38:35 +0530

    added env PORT and MONGO_URI update

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
 const Student = require("./models/Student");
 
 const app = express();
@@ -8,7 +9,7 @@ app.use(express.json());
 app.use(cors());
 
 // 1) MongoDB Connect
-mongoose.connect("mongodb+srv://Muskanjanghel:4jXnttQNwMOwIec4@cluster0.m3h66ug.mongodb.net/studentdb?retryWrites=true&w=majority", {
+mongoose.connect("process.env.MONGO_URI", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
